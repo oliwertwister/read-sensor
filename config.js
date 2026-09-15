493464687b586893ae018de366a8b97b2b3fbb0d
read-sensor <read-sensor@users.noreply.github.com>
2026-09-16 01:58:24 +0200

    Point dashboard to production API

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.READ_SENSOR_CONFIG = Object.freeze({
-  apiBase: "https://read-sensor-api.invalid",
+  apiBase: "https://read-sensor-api.read-sensor.workers.dev",
   defaultDevice: "sensor-node-01",
   defaultMetric: "cpu_temperature",
   historyLimit: 288,
